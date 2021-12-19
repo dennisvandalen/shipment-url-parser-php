12d361724a495689e395ec2a3053350515b3072f
dennisvandalen <dennisvandalen@users.noreply.github.com>
2021-12-19 01:25:45 +0000

    Fix styling

--- src/ShipmentUrlParser.php
+++ src/ShipmentUrlParser.php
@@ -55,7 +55,7 @@ class ShipmentUrlParser
         $result = [];
 
         foreach ($array as $item) {
-            if (!is_array($item)) {
+            if (! is_array($item)) {
                 $result[] = $item;
             } else {
                 $values = $depth === 1
